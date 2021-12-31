e6b8735732afc5a41918aacdaeaa291f6ac7e5e1
Henry Bradlow <henrybrad@gmail.com>
2021-12-31 05:37:47 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -60,7 +60,8 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
-        'rest_framework.authtoken'
+        'rest_framework.authtoken',
+        'drf_yasg'
     ]
 
     MIDDLEWARE = [
@@ -218,6 +219,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
 class Prod(Dev):
